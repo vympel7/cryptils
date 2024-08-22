21f00738fee4497c020ef627c92f7269d2356232
vympel7 <vitoaltieri04@gmail.com>
2024-08-22 13:01:15 +0200

    renaming

--- cryptils/AES/cbc.py
+++ cryptils/AES/cbc.py
@@ -1,12 +1,12 @@
 from .utils import *
 
-def recover_IV(encrypt_msg, decrypt_msg, msg: bytes = None):
+def recover_IV(encrypt_msg, decrypt_msg, msg = None):
     if msg is None:
         msg = b'0' * (2 * AES.block_size)
 
     data = blockify(encrypt_msg(msg))
     if len(data) < 2:
-        raise ValueError('At least 2 blocks are needed for this attack')
+        raise ValueError('At least 2 blocks are required for this attack.')
 
     payload = data[0] + data[0]
     decrypted = blockify(decrypt_msg(payload))
